57ec2465bc6496580847147b6f9b072a1c50482a
Francisco J. Aneiros Ramírez <Nightcrawler@MacBook-Pro-de-Francisco.local>
2013-11-30 17:10:54 +0000

    firts commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Murcielago::Application.config.secret_key_base = '16ff12ac40fe696a21bc621267c3dbca01e234a46128c559e3cbb58537b7a8bca3df25679f27f9345056e20bf8868b73be30c763f82e69b5b71787105e380ec9'
